797273f055ef00e0e148aff0f15aa9325a6fbf58
Mansur Zamaletdinov <zamaletdinov98@list.ru>
2022-11-25 14:37:44 +0300

    New Project

---
+++ Figures1/Figures1.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+
+class Figure {
+protected:
+	int sides_count = 0;
+	std::string name = "Фигура: ";
+public:
+	void get_sides_count() {
+		std::cout << "Количество сторон: " << std::endl << name << sides_count << std::endl;
+	}
+
+	
+};
+
+class Traingle : public Figure {
+public:
+	Traingle() {
+		sides_count = 3;
+		name = "Треугольник: ";
+		std::cout << name << sides_count << std::endl;
+	}
+};
+
+class Quadrangle : public Figure {
+public:
+	Quadrangle() {
+		sides_count = 4;
+		name = "Четырёхугольник: ";
+		std::cout << name << sides_count;
+	}
+};
+
+
+int main() {
+	setlocale(LC_ALL, "Ru");
+
+	Figure figure;
+	figure.get_sides_count();
+
+	Traingle traingle;
+	Quadrangle quadrangle;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+#include <iostream>
+
+class Figure {
+protected:
+	int sides_count;
+	std::string name;
+
+public:
+	int get_sides_count(int a) {
+		std::cout << "Количество сторон: " << a;
+	}
+
+	std::string get_name(std::string name1) {
+		std::cout << "Название Фигуры: " << name1;
+	}
+};
+
+class Triangle : public Figure {
+	Triangle(int a) {
+
+	}
+};
+
+class Quadrangle : public Figure {
+	Quadrangle(int a) {
+
+	}
+};
+*/
---
+++ Figures2/Figures2.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+
+
+class Figure {
+protected:
+	int a, b, c, d;
+	int A, B, C, D;
+};
+
+class Traingle : public Figure {
+protected:
+	int a = 10, b = 20, c = 30;
+	int A = 50, B = 60, C = 70;
+public:
+	Traingle() {
+		std::cout << "Треугольник: " << "\n" << "Строноны: " << "a=" << a << " b=" << b << " c=" << c << std::endl;
+		std::cout << "Углы: " << "A=" << A << " B=" << B << " C=" << C;
+	}
+};
+
+
+int main() {
+	setlocale(LC_ALL, "Ru");
+	Traingle traingle;
+	//traingle.get_a();
+}
+
+/*
+#include <iostream>
+
+
+class Figure {
+	
+};
+
+class Traingle : public Figure {
+protected:
+	int a = 10;
+	int b = 20;
+	int c = 30;
+
+	int A = 50;
+	int B = 60;
+	int C = 70;
+public:
+	int get_info() {
+		return a;
+	}
+	//void get_traingle() {
+	//	std::cout << "Треугольник:" << "\n" << "Стороны: " << "a=" << a << " b=" << b << " c=" << c;
+	//}
+};
+
+
+int print_info(Figure* figure) {
+	std::cout << a;
+}
+
+
+
+int main() {
+	setlocale(LC_ALL, "Ru");
+	Traingle traingle;
+	//std::cout << traingle.return_info();
+	//traingle.get_traingle();
+}
+*/
