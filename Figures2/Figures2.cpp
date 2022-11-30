283f4877add0f62820b6c5a247cb031f904ca5df
Mansur Zamaletdinov <101476332+MansurZamaletdinov08@users.noreply.github.com>
2022-11-30 21:17:44 +0300

    Update Figures2.cpp

--- Figures2/Figures2.cpp
+++ Figures2/Figures2.cpp
@@ -1,67 +1,48 @@
-﻿#include <iostream>
+#include <iostream>
 
-
-class Figure {
+class Figure
+{
 protected:
 	int a, b, c, d;
 	int A, B, C, D;
+public:
+	virtual int return_info() {
+		return a, b, c;
+	}
 };
 
-class Traingle : public Figure {
+
+class Traingle : public Figure
+{
 protected:
 	int a = 10, b = 20, c = 30;
 	int A = 50, B = 60, C = 70;
 public:
+
 	Traingle() {
-		std::cout << "Треугольник: " << "\n" << "Строноны: " << "a=" << a << " b=" << b << " c=" << c << std::endl;
-		std::cout << "Углы: " << "A=" << A << " B=" << B << " C=" << C;
+
+	}
+	int return_info() override {
+		return b;
 	}
 };
+ 
+void print_info(Figure* figure) {
+	figure->return_info();
+}
 
 
-int main() {
+
+int main(int argc, char** argv) {
 	setlocale(LC_ALL, "Ru");
+	Figure figure;
+	figure.return_info();
+
 	Traingle traingle;
-	//traingle.get_a();
+	traingle.return_info();
+
+	Figure* par_child = &traingle;
+	par_child->return_info();
+
+	print_info(par_child);
 }
-
-/*
-#include <iostream>
-
-
-class Figure {
-	
-};
-
-class Traingle : public Figure {
-protected:
-	int a = 10;
-	int b = 20;
-	int c = 30;
-
-	int A = 50;
-	int B = 60;
-	int C = 70;
-public:
-	int get_info() {
-		return a;
-	}
-	//void get_traingle() {
-	//	std::cout << "Треугольник:" << "\n" << "Стороны: " << "a=" << a << " b=" << b << " c=" << c;
-	//}
-};
-
-
-int print_info(Figure* figure) {
-	std::cout << a;
-}
-
-
-
-int main() {
-	setlocale(LC_ALL, "Ru");
-	Traingle traingle;
-	//std::cout << traingle.return_info();
-	//traingle.get_traingle();
-}
-*/
